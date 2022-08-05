f5dfeb2f949a6145eab0d11c16a55f32bb2af983
joshdarling <joshua.a.darling@gmail.com>
2022-08-04 22:18:48 -0600

    heroku seed.py postgres fix

--- seed.py
+++ seed.py
@@ -15,4 +15,4 @@ db.create_all()
 file_name = 'generator/states.csv'
 load = pd.read_csv(file_name)
 
-load.to_sql(name='states', con='DATABASE_URL', if_exists='append', index=false)
+load.to_sql(name='states', con='postgres://rwdeulyaxmrcda:167b888b467e918996cbf3f72e237315557ccba63783988fb02e859995bc1446@ec2-107-22-122-106.compute-1.amazonaws.com:5432/dfcvpit93qj6vh', if_exists='append', index=false)
