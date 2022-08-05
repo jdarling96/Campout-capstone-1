726ac8c940d9e7be43fd604bbbee40cd993e2feb
joshdarling <joshua.a.darling@gmail.com>
2022-08-04 22:14:51 -0600

    seed.py heroku postgres db

--- seed.py
+++ seed.py
@@ -1,9 +1,13 @@
+from distutils.command.config import config
 from fileinput import filename
+import os
 import pandas as pd
 from sqlalchemy import false
-from app import db
+from app import db, app
 from models import States
 
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql:///campout'))
 db.drop_all()
 
 db.create_all()
@@ -11,4 +15,4 @@ db.create_all()
 file_name = 'generator/states.csv'
 load = pd.read_csv(file_name)
 
-load.to_sql(name='states', con='postgresql:///campout', if_exists='append', index=false)
+load.to_sql(name='states', con='DATABASE_URL', if_exists='append', index=false)
