d0fb20f1e160dbc6ea49cd4319ee82a10cb3b10a
joshdarling <joshua.a.darling@gmail.com>
2022-08-04 23:03:31 -0600

    postgres to postgresql fix

--- seed.py
+++ seed.py
@@ -1,10 +1,14 @@
 from distutils.command.config import config
 from fileinput import filename
+import os
+import re
 import pandas as pd
 from sqlalchemy import false
 from app import db, app
 from models import States
- 
+
+postgres = os.environ.get('DATABASE_URL')
+
 db.drop_all()
 
 db.create_all()
@@ -12,4 +16,4 @@ db.create_all()
 file_name = 'generator/states.csv'
 load = pd.read_csv(file_name)
 
-load.to_sql(name='states', con='postgresql://rwdeulyaxmrcda:167b888b467e918996cbf3f72e237315557ccba63783988fb02e859995bc1446@ec2-107-22-122-106.compute-1.amazonaws.com:5432/dfcvpit93qj6vh', if_exists='append', index=false)
+load.to_sql(name='states', con=postgres if_exists='append', index=false)
